de5a74f6f657ac80b4ec9f689ecc90cd1e6fbdc3
Michael Vinogradov <interface.master@gmail.com>
2018-05-27 17:38:58 -0400

    prototyping the input form for registering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import md5 from 'crypto-js/md5';
+import sha256 from 'crypto-js/sha256';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+	title = 'MRCT';
+
+	email = '';
+	pass = '';
+	name = '';
+	role = 'admin';
+	salt = md5( new Date().getTime().toString() ).toString();
+	hash = '';
+
+	constructor() {
+	}
+
+	updateHash(event) {
+		this.pass = event.target.value;
+		this.hash = sha256( this.pass + this.salt ).toString();
+	}
+
+	register(event) {
+		let data = {
+			email: this.email,
+			pass: this.pass,
+			name: this.name,
+			role: this.role,
+			salt: this.salt,
+			hash: this.hash,
+		};
+		console.log("Registering...",data);
+		axios.post('http://localhost/register', data)
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+	}
+
 }
