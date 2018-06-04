e30d8178bc404f4565142023008e081230e11b0c
Michael Vinogradov <interface.master@gmail.com>
2018-06-04 00:45:27 -0400

    adding login form with axios functionality to get salt, send hash, and receive access token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
 	}
 
 	register(event) {
-		let data = {
+		const data = {
 			email: this.email,
 			pass: this.pass,
 			name: this.name,
@@ -39,12 +39,74 @@ export class AppComponent {
 		};
 		console.log("Registering...",data);
 		axios.post('http://localhost/register', data)
-		.then(function (response) {
+		.then( (response) => {
 			console.log(response);
 		})
-		.catch(function (error) {
-			console.log(error);
+		.catch( (error) => {
+			console.warn(error);
 		});
+	}
+
+	async login(event) {
+		let data = {
+			client_id: 'mrct',
+			client_secret: 'doascience',
+			scope: 'basic',
+			grant_type: 'password',
+			username: this.email,
+			salt: undefined,
+			hash: undefined,
+			id: undefined,
+			name: undefined,
+			role: undefined
+		}
+		// fetch the salt
+		const dataEmail = {
+			username: data.username
+		};
+		await axios.post('http://localhost/validate/email', dataEmail )
+		.then( (response) => {
+			data.salt = response.data.salt;
+			data.hash = sha256( this.pass + data.salt ).toString();
+		})
+		.catch( (error) => {
+			console.warn(error);
+		});
+		// fetch login
+		if( data.salt && data.hash ) {
+			const dataLogin = {
+				client_id: data.client_id,
+				client_secret: data.client_secret,
+				scope: data.scope,
+				grant_type: data.grant_type,
+				username: data.username,
+				password: data.hash
+			};
+			await axios.post('http://localhost/validate/login', dataLogin)
+			.then( (response) => {
+				data.id = response.data.id;
+				data.name = response.data.name;
+				data.role = response.data.role;
+			})
+			.catch( (error) => {
+				console.warn(error);
+			});
+		}
+		// validate
+		if( data.id ) {
+			// logged in
+			const user = {
+				id: data.id,
+				name: data.name,
+				email: data.username,
+				role: data.role
+			}
+			console.log("%cSUCCESS!","color:green;",data)
+			console.log("%cdestructured:","color:purple",user)
+		} else {
+			// error
+			console.log("%cFAIL!","color:red;",data)
+		}
 	}
 
 }
