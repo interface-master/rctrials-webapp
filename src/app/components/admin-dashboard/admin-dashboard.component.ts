e0a2a3089e551fcc3472c96ad2d227f83379f1ff
Michael Vinogradov <interface.master@gmail.com>
2019-01-13 14:17:55 -0500

    added list of available trials to the admin dashboard

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-// import { SessionService } from "../../services/session.service";
+import { SessionService } from '../../services/session.service';
 
+import axios from 'axios';
 
 @Component({
 	selector: 'admin-dashboard',
@@ -11,12 +12,51 @@ import { Router } from '@angular/router';
 })
 export class AdminDashboardComponent implements OnInit {
 
+	private trials: Trial[];
+
 	constructor(
-		// private session: SessionService,
+		private session: SessionService
 		// private router: Router
 	) { }
 
-	ngOnInit() {
+	async ngOnInit() {
+		this.trials = <Trial[]> await this.getTrialList();
+		console.log('this:', this);
 	}
 
+	async getTrialList() {
+		// send data
+		const config = {
+			headers: {'Authorization': `Bearer ${ this.session.parseCookie( 'access_token' ) }`}
+		};
+		return await axios.get('http://localhost/user/trials', config) // TODO: remove hard-coded URLs into a service
+		.then( (response) => {
+			return response.data;
+			// if( response.data.status !== 200 ) {
+			// 	let message = response.data.message || "No error message was specified.";
+			// 	this.session.openDialog( "Creating New Trial Failed", message );
+			// } else {
+			// 	this.session.openDialog( "New Trial Created", `Your trial has been created. The ID of your trial is: ${response.data.tid}.\nIt has ${response.data.groups} groups, ${response.data.surveys} surveys, and ${response.data.questions} questions.` );
+			// }
+			// console.log(response);
+		})
+		.catch( (error) => {
+			console.warn(error);
+			return [];
+		});
+	}
+
+}
+
+type Trial = {
+	tid: string,
+	title: string,
+	regopen: Date,
+	regclose: Date,
+	trialstart: Date,
+	trialend: Date,
+	trialtype: string,
+	timezone: string,
+	created: Date,
+	updated: Date
 }
