9e9ccf4d01cd43de6dd9a5d34eafc576dbb96287
Michael Vinogradov <interface.master@gmail.com>
2019-01-21 10:50:21 -0500

    adding api to url root

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ApiService {
-	private _apiRoot:string = "http://localhost/";
+	private _apiRoot:string = "http://localhost/api/";
 
 	public login:string = `${this._apiRoot}validate/login`;
 	public register:string = `${this._apiRoot}register`;
