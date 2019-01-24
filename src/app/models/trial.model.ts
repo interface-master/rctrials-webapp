e5c172ed8d1dc6ef5eb2a003f18ce03ffa71d6ea
Michael Vinogradov <interface.master@gmail.com>
2019-01-23 20:18:48 -0500

    adding trial model

---
+++ src/app/models/trial.model.ts
@@ -0,0 +1,12 @@
+export type Trial = {
+	tid: string,
+	title: string,
+	regopen: Date,
+	regclose: Date,
+	trialstart: Date,
+	trialend: Date,
+	trialtype: string,
+	timezone: string,
+	created: Date,
+	updated: Date
+}
