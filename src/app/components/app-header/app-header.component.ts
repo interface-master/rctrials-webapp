4b6f9f0e786e131e07201e95b472ffad85761f10
Michael Vinogradov <interface.master@gmail.com>
2019-02-21 23:28:01 -0500

    adding dynamic menu items for logged in users

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+import { SessionService } from "../../services/session.service";
+
 @Component({
 	selector: 'app-header',
 	templateUrl: './app-header.component.html',
@@ -8,16 +10,27 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppHeaderComponent implements OnInit {
 	private _menu:boolean = false;
+	private _signedIn:boolean = false;
 
 	constructor(
-		private router: Router
+		private router: Router,
+		private sessionService: SessionService,
 	) {
 		const self = this;
+		// close menu when routing
 		router.events.subscribe((val) => {
 			if( val instanceof NavigationEnd ) {
 				self.closeMenu();
 			}
 		});
+		// hide/show menu items when logged in
+		sessionService.currentUserInfo.subscribe(
+			userInfo => {
+				if( userInfo.uid ) {
+					self._signedIn = true;
+				}
+			}
+		)
 	}
 
 	ngOnInit() {
