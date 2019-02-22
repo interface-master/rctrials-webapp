1a06e959ae7f01a1c9d2df0d53314b1d83582518
Michael Vinogradov <interface.master@gmail.com>
2019-02-21 23:27:36 -0500

    cleanup

--- src/app/components/app-footer/app-footer.component.ts
+++ src/app/components/app-footer/app-footer.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { SessionService } from "../../services/session.service";
-
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-footer',
 	templateUrl: './app-footer.component.html',
 	styleUrls: ['./app-footer.component.scss']
 })
-export class AppFooterComponent implements OnInit {
-
-	constructor(
-		private session: SessionService
-	) { }
-
-	ngOnInit() {
-	}
-
+export class AppFooterComponent {
+	constructor() {}
 }
