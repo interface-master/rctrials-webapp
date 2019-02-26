27b2934ea145bcb5301559a8ee11ccbd235d7574
Michael Vinogradov <interface.master@gmail.com>
2019-02-25 21:15:21 -0500

    adding charts to trial details page

--- src/app/components/admin-trial-details/admin-trial-details.component.ts
+++ src/app/components/admin-trial-details/admin-trial-details.component.ts
@@ -6,6 +6,7 @@ import { ApiService } from '../../services/api.service';
 import { SessionService } from "../../services/session.service";
 
 import axios from 'axios';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -17,7 +18,12 @@ export class AdminTrialDetailsComponent implements OnInit, OnDestroy {
 	private _tid:string;
 	private _routeSubscriber:any;
 
+	private charts = {};
+	private chartX = null;
+	private _chartsRendered = false;
+
 	public trial:Trial;
+	private questions = [];
 
 	constructor(
 		private api: ApiService,
@@ -71,7 +77,12 @@ export class AdminTrialDetailsComponent implements OnInit, OnDestroy {
 				enrichedTrial.totalSubjects = enrichedTrial.subjects.reduce( (a,c) => {
 					return a + Number(c.subjects);
 				}, 0);
+				// consolidate questions
+				this.questions = enrichedTrial.surveys
+						.map( s => s.questions.reduce( (a,c) => [...a, c], [] ) )
+						.reduce( (a,c) => [...a, ...c], [] );
 				// fin
+				console.log("admin TRIAL details:",enrichedTrial);
 				return enrichedTrial;
 			}
 		})
@@ -79,6 +90,111 @@ export class AdminTrialDetailsComponent implements OnInit, OnDestroy {
 			console.warn(error);
 			return [];
 		});
+	}
+
+	configureCharts() {
+		// generate charts from answer data
+		for( let q = 0; q < this.questions.length; q++ ) {
+			if( this.questions[q].totals ) {
+				// gather labels and values
+				let dataLabels = [], dataPoints = [];
+				this.questions[q].totals.map( v => {
+					dataLabels.push( this.questions[q].options.split('|')[v.text] || v.text );
+					dataPoints.push( { y : parseInt(v.count) } );
+				});
+				let obj = {
+					data: dataPoints,
+					borderColor: '#00c9',
+					backgroundColor: '#00c9',
+				}
+				// build chart
+				let qid = this.questions[q].qid;
+				this.charts[qid] = new Chart(`chart-${qid}`,{
+					type: "bar",
+					data: {
+						labels: dataLabels,
+						datasets: [obj]
+					},
+					options: {
+						legend: {
+							display: false
+						},
+						scales: {
+							yAxes: [{
+								ticks: {
+									beginAtZero: true,
+									callback: (val) => { if( val % 1 === 0 ) { return val; } }
+								}
+							}]
+						}
+					}
+				});
+			}
+			if( this.questions[q].answers ) {
+				let oldUID = '', dataPoints = [], datasets = [];
+				for( let v = 0; v < this.questions[q].answers.length; v++ ) {
+					let a = this.questions[q].answers[v];
+					if( oldUID != a.uid ) {
+						let r = (23 * datasets.length); // prime so that goes through all the shades
+						datasets.push({
+							data: dataPoints,
+							borderColor: 'hsla('+r+',100%,50%,0.625)',
+							backgroundColor: 'hsla('+r+',100%,50%,0.625)',
+							fill: false
+						});
+						oldUID = a.uid;
+						dataPoints = [];
+					}
+					dataPoints.push({ y: parseInt(a.text), x: a.timestamp });
+				}
+				// build chart
+				let qid = this.questions[q].qid;
+				this.charts[qid] = new Chart(`chart-${qid}`,{
+					type: "line",
+					data: {
+						datasets: datasets
+					},
+					options: {
+						legend: {
+							display: false
+						},
+						scales: {
+							yAxes: [{
+								ticks: {
+									beginAtZero: true,
+									callback: (val) => { if( val % 1 === 0 ) { return val; } }
+								}
+							}],
+							xAxes: [{
+								type: 'time',
+								time: {
+									unit: 'day'
+								}
+							}]
+						}
+					}
+				});
+			}
+		}
+		console.log('questions:', this.questions);
+		console.log("admin trial details CHARTS:", this.charts);
+	}
+
+	clickedQuestion(event:any) {
+		event.currentTarget.classList.toggle('zoomed');
+	}
+
+	ngAfterContentChecked() {
+		if( !this._chartsRendered && this.trial ) {
+			let nodes = document.querySelectorAll('[id^=chart-]').length;
+			console.log("checking DOM nodes:", nodes );
+			if( nodes == this.questions.length ) {
+				// RENDER CHARTS
+				console.log("RENDERING NOW");
+				this.configureCharts();
+				this._chartsRendered = true;
+			}
+		}
 	}
 
 }
