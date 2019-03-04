136b16a8eb68819f73dbd8552e6f30af6d7d48ea
Michael Vinogradov <interface.master@gmail.com>
2019-03-04 06:55:24 -0500

    adding a CanDeactivate guard to prevent accidentally exiting the New Trial form

---
+++ src/app/services/can-deactivate.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+	CanDeactivate,
+	ActivatedRouteSnapshot,
+	RouterStateSnapshot
+} from '@angular/router';
+import { Router } from '@angular/router';
+
+import { AdminNewTrialComponent } from '../components/admin-new-trial/admin-new-trial.component';
+import { DialogService } from './dialog.service';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class CanDeactivateGuard implements CanDeactivate<AdminNewTrialComponent> {
+	constructor(
+		public dialog: DialogService,
+		private router: Router
+	) { }
+
+	canDeactivate(
+		component: AdminNewTrialComponent,
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot
+	):Observable<boolean> | boolean {
+		if( component.newTrialForm.touched ) {
+			return this.dialog.confirm(
+				"Discard Changes?",
+				"There are unsaved changes in the form, are you sure you wish to discard them?"
+			);
+		}
+		return true;
+	}
+}
