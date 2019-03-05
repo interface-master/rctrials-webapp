85d3a2951fccb5fb178548ecd0d028ff07abb70c
Michael Vinogradov <interface.master@gmail.com>
2019-03-05 16:33:08 -0500

    extracting summary step

--- src/app/components/admin-new-trial/summary.step.component.ts
+++ src/app/components/admin-new-trial/summary.step.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup, FormArray } from '@angular/forms';
 
+import { AdminNewTrialComponent } from './admin-new-trial.component';
+
 @Component({
 	selector: 'step-summary',
 	templateUrl: './summary.step.component.html',
 	styleUrls: ['./admin-new-trial.component.scss']
 })
 export class AdminNewTrialSummaryStepComponent {
-	@Input() parentForm: FormGroup;
+	@Input() parent: AdminNewTrialComponent;
+	// @Input() parentForm: FormGroup;
 	@Input() groups: FormArray;
 	@Input() surveys: FormArray;
+	@Input() basicInfoStepForm: FormGroup;
+	@Input() trialGroupsStepForm: FormGroup;
+	@Input() surveyQuestionsStepForm: FormGroup;
 }
