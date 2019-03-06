85cb307f674b7d9fa8ba4b86bb369c363a76829c
Michael Vinogradov <interface.master@gmail.com>
2019-03-06 14:50:07 -0500

    adding date validation (must be > previous); and polishing ui

--- src/app/components/admin-new-trial/basic-info.step.component.ts
+++ src/app/components/admin-new-trial/basic-info.step.component.ts
@@ -12,6 +12,31 @@ export class AdminNewTrialBasicInfoStepComponent {
 	@Input() parent: AdminNewTrialComponent;
 	@Input() parentForm: FormGroup;
 
+	private start_at = [
+		new Date(), // min for reg open
+		new Date(), // min for reg close
+		new Date(), // min for trial start
+		new Date()  // min for trial end
+	];
+
+	updateDates(event) {
+		console.log('update dates event:',event);
+		console.log("just in case:",this.parentForm);
+		let tomorrow = new Date();
+		tomorrow.setDate(event.value.getDate() + 1);
+		switch( event.targetElement.name ) {
+			case 'regopen':
+				this.start_at[1] = tomorrow;
+				break;
+			case 'regclose':
+				this.start_at[2] = event.value;
+				break;
+			case 'trialstart':
+				this.start_at[3] = tomorrow;
+				break;
+		}
+	}
+
 	validate(event) {
 		console.log('validating basic info',this.parentForm);
 		// Object.keys( this.parentForm.controls ).forEach( key => {
