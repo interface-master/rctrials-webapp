0ba9ae1b1125c2847bc136c7aeab802a8aa5c100
Michael Vinogradov <interface.master@gmail.com>
2019-07-17 20:49:51 -0400

    adding documentation

--- src/app/components/documentation/doc.developers.component.ts
+++ src/app/components/documentation/doc.developers.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+
+import { DialogService } from '../../services/dialog.service';
+
 @Component({
 	selector: 'doc-developers',
 	templateUrl: './doc.developers.component.html',
 	styleUrls: ['./documentation.component.scss']
 })
 export class DocDevelopersComponent {
-	constructor() {}
+	constructor(
+		private dialog: DialogService
+	) {}
+
+	imagePopup(event) {
+		console.log('popup');
+		if(event.preventDefault) event.preventDefault();
+		const ret:Subscription = this.dialog.image( '/assets/img/docs/postman_collection.gif' )
+		.subscribe( val => {
+			ret.unsubscribe();
+		});
+	}
 }
