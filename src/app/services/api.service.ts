cb70c8cdb0cdb04e2991acab380de7894733672f
Michael Vinogradov <interface.master@gmail.com>
2020-01-17 11:32:29 -0500

    correct localhost url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ApiService {
-	private _apiRoot:string = "http://rctrials.tk/api/";
+	private _apiRoot:string = "http://localhost:8081/api/";
 
 	public login:string = `${this._apiRoot}validate/login`;
 	public newTrial:string = `${this._apiRoot}new/trial`;
